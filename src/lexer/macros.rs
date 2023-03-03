01d30eeb425773e0e4323e0da762a0727964bdc6
Abiria <abiria.dev@gmail.com>
2023-03-03 18:50:22 +0900

    fix: add $ symbol

--- src/lexer/macros.rs
+++ src/lexer/macros.rs
@@ -3,7 +3,7 @@ macro_rules! char_token {
 	($ident: ident: $char: literal -> $tok: expr) => {
 		fn $ident(input: &str) -> nom::IResult<&str, Token> {
 			nom::combinator::value(
-				tok,
+				$tok,
 				nom::character::complete::char($char),
 			)(input)
 		}
