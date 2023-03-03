dfc970ed9c0b60fc9a97970265d0468fd742e887
Abiria <abiria.dev@gmail.com>
2023-03-03 18:51:48 +0900

    feat: add parser for each (but not all) token

--- src/lexer/parsers.rs
+++ src/lexer/parsers.rs
@@ -1,9 +1,11 @@
 use super::token::Token;
-use nom::IResult;
 
-use nom::character::complete::char;
-use nom::combinator::value;
-
-fn parse_right(input: &str) -> IResult<&str, Token> {
-	value(Token::Right, char('?'))(input)
-}
+char_token! { parse_right: '?' -> Token::Right }
+char_token! { parse_left: '!' -> Token::Left }
+char_token! { parse_inc: '냥' -> Token::Inc }
+char_token! { parse_dec: '냐' -> Token::Dec }
+char_token! { parse_out: '.' -> Token::Out }
+char_token! { parse_in: ',' -> Token::In }
+char_token! { parse_jump_right: '~' -> Token::JumpRight }
+char_token! { parse_jump_left: '-' -> Token::JumpLeft }
+char_token! { parse_debug: '뀨' -> Token::Debug }
