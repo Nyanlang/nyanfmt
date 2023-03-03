6da3c0106ea854c5ff6bbdce02d560856c339dbb
Abiria <abiria.dev@gmail.com>
2023-03-03 21:08:37 +0900

    feat: fix some move issue then implement IFormatter

--- src/formatter/formatter.rs
+++ src/formatter/formatter.rs
@@ -1,6 +1,6 @@
-// use crate::lexer::Token;
-//
-// use super::Span;
+use crate::lexer::Token;
+
+use super::Span;
 
 trait IFormatter<I>
 where
@@ -9,7 +9,31 @@ where
 	type Input;
 	type Output;
 
-	fn get_code(&self) -> I;
+	fn get_code(&self) -> &I;
 
 	fn format<O>(&mut self, stream_out: &mut Vec<Self::Output>);
+}
+
+struct Formatter<I>
+where
+	I: Iterator<Item = Token>,
+{
+	iter: I,
+}
+
+impl<I> IFormatter<I> for Formatter<I>
+where
+	I: Iterator<Item = Token>,
+{
+	type Input = Token;
+
+	type Output = Span;
+
+	fn get_code(&self) -> &I {
+		&self.iter
+	}
+
+	fn format<O>(&mut self, stream_out: &mut Vec<Self::Output>) {
+		todo!()
+	}
 }
