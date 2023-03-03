4173f32d6302de08f3a3d523a28059cf36402bb5
Abiria <abiria.dev@gmail.com>
2023-03-03 18:55:53 +0900

    feat: add the parser for all (currently available) tokens

--- src/lexer/parsers.rs
+++ src/lexer/parsers.rs
@@ -1,4 +1,7 @@
+use nom::IResult;
+
 use super::token::Token;
+use nom::branch::alt;
 
 char_token! { parse_right: '?' -> Token::Right }
 char_token! { parse_left: '!' -> Token::Left }
@@ -9,3 +12,17 @@ char_token! { parse_in: ',' -> Token::In }
 char_token! { parse_jump_right: '~' -> Token::JumpRight }
 char_token! { parse_jump_left: '-' -> Token::JumpLeft }
 char_token! { parse_debug: '뀨' -> Token::Debug }
+
+pub fn parse_token(input: &str) -> IResult<&str, Token> {
+	alt((
+		parse_right,
+		parse_left,
+		parse_inc,
+		parse_dec,
+		parse_out,
+		parse_in,
+		parse_jump_right,
+		parse_jump_left,
+		parse_debug,
+	))(input)
+}
