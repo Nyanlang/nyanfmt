cb4b1a2b44938234ec34b75ea20e288a2aae0480
Abiria <abiria.dev@gmail.com>
2023-03-04 21:02:19 +0900

    refactor: rewrite parsers to use `tag` combinator

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,13 +1,16 @@
 struct Root;
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum HeadTok {
 	Inc,
 	Dec,
 	Debug,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct Head(pub Vec<HeadTok>);
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum BodyTok {
 	Out,
 	In,
@@ -15,13 +18,16 @@ pub enum BodyTok {
 	JumpLeft,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct Body(pub Vec<BodyTok>);
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum TailTok {
 	Right,
 	Left,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct Tail(pub Vec<TailTok>);
 
 pub struct Word {
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,62 +1,71 @@
-use nom::{bytes::complete::is_a, combinator::map, IResult, InputIter};
-
 use super::ast::*;
 use crate::lexer::{Token::*, TokenStream};
+use nom::{
+	branch::alt,
+	bytes::complete::tag,
+	combinator::{map, value},
+	multi::many1,
+	IResult,
+};
+
+fn parse_inc(input: TokenStream) -> IResult<TokenStream, HeadTok> {
+	value(HeadTok::Inc, tag(&Inc))(input)
+}
+
+fn parse_dec(input: TokenStream) -> IResult<TokenStream, HeadTok> {
+	value(HeadTok::Dec, tag(&Dec))(input)
+}
+
+fn parse_debug(input: TokenStream) -> IResult<TokenStream, HeadTok> {
+	value(HeadTok::Debug, tag(&Debug))(input)
+}
+
+fn parse_out(input: TokenStream) -> IResult<TokenStream, BodyTok> {
+	value(BodyTok::Out, tag(&Out))(input)
+}
+
+fn parse_in(input: TokenStream) -> IResult<TokenStream, BodyTok> {
+	value(BodyTok::In, tag(&In))(input)
+}
+
+fn parse_jump_righy(input: TokenStream) -> IResult<TokenStream, BodyTok> {
+	value(BodyTok::JumpRight, tag(&JumpRight))(input)
+}
+
+fn parse_jump_left(input: TokenStream) -> IResult<TokenStream, BodyTok> {
+	value(BodyTok::JumpLeft, tag(&JumpLeft))(input)
+}
+
+fn parse_right(input: TokenStream) -> IResult<TokenStream, TailTok> {
+	value(TailTok::Right, tag(&Right))(input)
+}
+
+fn parse_left(input: TokenStream) -> IResult<TokenStream, TailTok> {
+	value(TailTok::Left, tag(&Left))(input)
+}
 
 fn parse_head(input: TokenStream) -> IResult<TokenStream, Head> {
 	map(
-		is_a(TokenStream::from(
-			&[Inc, Dec, Debug][..],
-		)),
-		|o: TokenStream| {
-			Head(
-				o.iter_elements()
-					.map(|t| match t {
-						Inc => HeadTok::Inc,
-						Dec => HeadTok::Dec,
-						Debug => HeadTok::Debug,
-						_ => unreachable!(),
-					})
-					.collect::<Vec<_>>(),
-			)
-		},
+		many1(alt((parse_inc, parse_dec, parse_debug))),
+		Head,
 	)(input)
 }
 
 fn parse_body(input: TokenStream) -> IResult<TokenStream, Body> {
 	map(
-		is_a(TokenStream::from(
-			&[Out, In, JumpRight, JumpLeft][..],
-		)),
-		|o: TokenStream| {
-			Body(
-				o.iter_elements()
-					.map(|t| match t {
-						Out => BodyTok::Out,
-						In => BodyTok::In,
-						JumpRight => BodyTok::JumpRight,
-						JumpLeft => BodyTok::JumpLeft,
-						_ => unreachable!(),
-					})
-					.collect::<Vec<_>>(),
-			)
-		},
+		many1(alt((
+			parse_out,
+			parse_in,
+			parse_jump_righy,
+			parse_jump_left,
+		))),
+		Body,
 	)(input)
 }
 
 fn parse_tail(input: TokenStream) -> IResult<TokenStream, Tail> {
 	map(
-		is_a(TokenStream::from(&[Right, Left][..])),
-		|o: TokenStream| {
-			Tail(
-				o.iter_elements()
-					.map(|t| match t {
-						Right => TailTok::Right,
-						Left => TailTok::Left,
-						_ => unreachable!(),
-					})
-					.collect::<Vec<_>>(),
-			)
-		},
+		many1(alt((parse_right, parse_left))),
+		Tail,
 	)(input)
 }
