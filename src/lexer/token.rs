442fc9b1e949cd966b4cba31dc81678019668660
Abiria <abiria.dev@gmail.com>
2023-03-04 18:49:56 +0900

    feat: implement InputLength for TokenStream

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,3 +1,5 @@
+use nom::{InputLength, InputTake};
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
 	Right,
@@ -16,4 +18,10 @@ pub enum Token {
 #[derive(Debug, PartialEq, Clone)]
 pub struct TokenStream<'a> {
 	stream: &'a [Token],
+}
+
+impl<'a> InputLength for TokenStream<'a> {
+	fn input_len(&self) -> usize {
+		self.stream.len()
+	}
 }
