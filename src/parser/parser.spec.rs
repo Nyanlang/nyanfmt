53d5bd2a59892f8c742b38c66ae18000e277ee73
Abiria <abiria.dev@gmail.com>
2023-03-04 23:20:23 +0900

    test: add unit tests for word parser

--- src/parser/parser.spec.rs
+++ src/parser/parser.spec.rs
@@ -144,3 +144,119 @@ fn must_fail_to_parse_tail_if_there_are_no_matched_tokens() {
 		))
 	);
 }
+
+#[test]
+fn must_parse_word() {
+	let code = TokenStream::from(
+		&[Out, JumpRight, In, JumpLeft, Left, Out, Debug][..],
+	);
+
+	assert_eq!(
+		parse_word(code),
+		Ok((
+			TokenStream::from(&[Out, Debug][..]),
+			Word {
+				head: None,
+				body: Some(Body(vec![
+					BodyTok::Out,
+					BodyTok::JumpRight,
+					BodyTok::In,
+					BodyTok::JumpLeft
+				])),
+				tail: Some(Tail(vec![TailTok::Left]))
+			}
+		))
+	)
+}
+
+#[test]
+fn must_parse_word2() {
+	let code = TokenStream::from(
+		&[Debug, Inc, JumpRight, In, JumpLeft, Left, Out, Debug][..],
+	);
+
+	assert_eq!(
+		parse_word(code),
+		Ok((
+			TokenStream::from(&[Out, Debug][..]),
+			Word {
+				head: Some(Head(
+					vec![HeadTok::Debug, HeadTok::Inc,]
+				)),
+				body: Some(Body(vec![
+					BodyTok::JumpRight,
+					BodyTok::In,
+					BodyTok::JumpLeft
+				])),
+				tail: Some(Tail(vec![TailTok::Left]))
+			}
+		))
+	)
+}
+
+#[test]
+fn parse_word_only_match_head() {
+	let code = TokenStream::from(&[Inc, Inc][..]);
+
+	assert_eq!(
+		parse_word(code),
+		Ok((
+			TokenStream::new(),
+			Word {
+				head: Some(Head(vec![HeadTok::Inc, HeadTok::Inc,])),
+				body: None,
+				tail: None
+			}
+		))
+	)
+}
+
+#[test]
+fn parse_word_only_match_body() {
+	let code = TokenStream::from(&[Out, JumpLeft, Dec, JumpLeft][..]);
+
+	assert_eq!(
+		parse_word(code),
+		Ok((
+			TokenStream::from(&[Dec, JumpLeft][..]),
+			Word {
+				head: None,
+				body: Some(Body(vec![
+					BodyTok::Out,
+					BodyTok::JumpLeft
+				])),
+				tail: None
+			}
+		))
+	)
+}
+
+#[test]
+fn parse_word_only_match_tail() {
+	let code = TokenStream::from(&[Left, Left, Debug][..]);
+
+	assert_eq!(
+		parse_word(code),
+		Ok((
+			TokenStream::from(&[Debug][..]),
+			Word {
+				head: None,
+				body: None,
+				tail: Some(Tail(vec![TailTok::Left, TailTok::Left]))
+			}
+		))
+	)
+}
+
+#[test]
+fn must_fail_to_parse_word_if_input_is_empty() {
+	let code = TokenStream::new();
+
+	assert_eq!(
+		parse_word(code).finish(),
+		Err(nom::error::Error::new(
+			TokenStream::new(),
+			ErrorKind::Verify
+		))
+	);
+}
