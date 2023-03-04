8778e3f3e0e7c7e0b6071ea957102fcfde14d0f0
Abiria <abiria.dev@gmail.com>
2023-03-04 19:07:57 +0900

    feat: implement InputTake and InputIter traits

--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,4 +1,6 @@
-use nom::{InputLength, InputTake};
+use std::{iter::Enumerate, slice::Iter};
+
+use nom::{InputIter, InputLength, InputTake, Needed};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
@@ -32,8 +34,51 @@ impl<'a> From<&'a [Token]> for TokenStream<'a> {
 	}
 }
 
+impl<'a> InputIter for TokenStream<'a> {
+	type Item = &'a Token;
+
+	type Iter = Enumerate<Self::IterElem>;
+
+	type IterElem = Iter<'a, Token>;
+
+	fn iter_indices(&self) -> Self::Iter {
+		self.iter_elements().enumerate()
+	}
+
+	fn iter_elements(&self) -> Self::IterElem {
+		self.stream.iter()
+	}
+
+	fn position<P>(&self, predicate: P) -> Option<usize>
+	where
+		P: Fn(Self::Item) -> bool,
+	{
+		self.iter_elements().position(predicate)
+	}
+
+	fn slice_index(&self, count: usize) -> Result<usize, nom::Needed> {
+		if self.input_len() >= count {
+			Ok(count)
+		} else {
+			Err(Needed::Unknown)
+		}
+	}
+}
+
 impl<'a> InputLength for TokenStream<'a> {
 	fn input_len(&self) -> usize {
 		self.stream.len()
+	}
+}
+
+impl<'a> InputTake for TokenStream<'a> {
+	fn take(&self, count: usize) -> Self {
+		Self::from(&self.stream[..count])
+	}
+
+	fn take_split(&self, count: usize) -> (Self, Self) {
+		match self.stream.split_at(count) {
+			(l, r) => (Self::from(r), Self::from(l)),
+		}
 	}
 }
