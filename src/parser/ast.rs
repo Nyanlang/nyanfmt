82577359e0db8636d63ea9dba5953c929fc4a776
Abiria <abiria.dev@gmail.com>
2023-03-04 12:47:09 +0900

    feat: add basic AST structs

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,1 +1,25 @@
 struct Root;
+
+enum Head {
+	Inc,
+	Dec,
+	Debug,
+}
+
+enum Body {
+	Out,
+	In,
+	JumpRight,
+	JumpLeft,
+}
+
+enum Tail {
+	Right,
+	Left,
+}
+
+struct Word {
+	Head: Head,
+	Body: Body,
+	Tail: Tail,
+}
