0b502ddb8b154c23227bd3f65b4cb1b301504cf7
Abiria <abiria.dev@gmail.com>
2023-03-12 17:50:44 +0900

    refactor: improve `parse_token!` to accept genertic `E`

--- src/parser/macro.rs
+++ src/parser/macro.rs
@@ -1,6 +1,11 @@
 macro_rules! parse_token {
 	($ident: ident: $token: expr => $ast_var: expr => $ast: ty) => {
-		fn $ident(input: TokenStream) -> nom::IResult<TokenStream, $ast> {
+		fn $ident<'a, E>(
+			input: TokenStream<'a>,
+		) -> nom::IResult<TokenStream<'a>, $ast, E>
+		where
+			E: nom::error::ParseError<TokenStream<'a>>,
+		{
 			nom::combinator::value(
 				$ast_var,
 				nom::bytes::complete::tag(&$token),
